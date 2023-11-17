7ea15035650ec21f872e17f3b65fef170e5c9735
Nikita Morgun <126413162+troubleShooter239@users.noreply.github.com>
2023-11-17 19:43:21 +0300

    Add  Vigenere

--- ciphers.py
+++ ciphers.py
@@ -94,3 +94,58 @@ class Vigenere(CryptBase):
             self.matrix[0][self.matrix[adjusted_ind[i]].index(c)] 
             for i, c in enumerate(msg)
         )
+####################################################################
+FIX BIGRAMS!
+####################################################################
+class Playfer(CryptBase):
+    def __init__(self,
+                 key: Optional[str] = None) -> None:         
+        self.alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"  # i = j, j = i
+        self.size = (5, 5)
+        self.key = key or ""
+        self.matrix = self.generate_matrix(self.key)
+
+    def generate_matrix(self, key: str) -> list[list[str]]:
+        cols = self.size[1]
+        text = ""
+        for c in key + self.alphabet:
+            if c not in text:
+                text += c  
+        return [text[i:i + cols] for i in range(0, len(text), cols)]
+    
+    def find_position(self, char: str) -> tuple[int, int]:
+        for i, row in enumerate(self.matrix):
+            try:
+                j = row.index(char)
+                return i, j
+            except ValueError: ...
+        return -1, -1
+        
+    def process_pair(self, char1: str, char2: str, direction: int) -> str:
+        row1, col1 = self.find_position(char1)
+        row2, col2 = self.find_position(char2)
+
+        if row1 == row2:
+            col1, col2 = (col1 + direction) % self.size[1], (col2 + direction) % self.size[1]
+        elif col1 == col2:
+            row1, row2 = (row1 + direction) % self.size[0], (row2 + direction) % self.size[0]
+        else:
+            col1, col2 = col2, col1
+
+        return self.matrix[row1][col1] + self.matrix[row2][col2]
+        
+    def encrypt(self, msg: str) -> str:
+        msg = msg.replace(' ', '_')
+        text = ""
+        for i in range(0, len(msg), 2):
+            char1 = msg[i]
+            char2 = msg[i + 1] if i + 1 < len(msg) else 'Х'
+            text += self.process_pair(char1, char2, 1)
+        return text
+
+    def decrypt(self, msg: str) -> str:
+        text = ""
+        for i in range(0, len(msg), 2):
+            char1, char2 = msg[i], msg[i + 1]
+            text += self.process_pair(char1, char2, 1)
+        return text.replace('_', ' ')
